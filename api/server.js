fdf4071bacf1ab01b2505d9e4224fdbeb6729834
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2019-12-16 17:06:25 -0600

    set up endpoints

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express =require('express');
+const apiRouter = require('./api-router.js');
+
+const Middleware = require('./middleware');
 
 const server = express();
 
-server.use(express.json());
+Middleware(server);
 
-// server.use('/api');
+server.use('/api', apiRouter);
+
 
 module.exports = server;
