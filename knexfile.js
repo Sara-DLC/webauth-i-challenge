250f8d593a7d8856dd85e096f42e02e5342b3037
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2019-12-16 15:30:33 -0600

    set up server

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,22 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './database/auth_users/db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds : {
+      directory: './database/seeds',
+    },
   },
 };
